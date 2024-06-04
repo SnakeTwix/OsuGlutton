c824e527214bb02d721d85bccdacfbc1b024e380
Alexey <poqe64@gmail.com>
2024-06-04 04:59:09 +0300

    Remove dist from git

--- dist/player/player.d.ts
+++
@@ -1,7 +0,0 @@
-import { IPlayerConfig } from "./playerConfig";
-export declare class Player {
-    displayName: string;
-    playerServers: IPlayerConfig[];
-    constructor(displayName: string, playerServers: IPlayerConfig[]);
-    fetchAllScores(): Promise<{}[]>;
-}
--- dist/servers/bancho.server.d.ts
+++
@@ -1,5 +0,0 @@
-import { OsuServer } from "./osu.server";
-export declare class ServerBancho extends OsuServer {
-    constructor();
-    fetchTopScores(userId: string): Promise<{}[]>;
-}
